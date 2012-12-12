aaa361facd0a264116b1b843d76201d0be75bb2d
Tass Iliopoulos <tass@iliopoulos.com.au>
2012-12-12 07:52:46 -0500

    Improved router and handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,25 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
   
   var content = "empty";
   
   exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
   });
 
   return content;
 }
 
-function upload() {
-  console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+function upload(response) {
+    console.log("Request handler 'upload' was called.");
+    
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello upload");
+    response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -4,13 +4,11 @@ var url = require("url");
 function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
+                
+        console.log("Request for " + pathname);
         
-        var content = route(handle, pathname);
+        route(handle, pathname, response);
         
-        console.log("Request received");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content);
-        response.end();
     }
     
     http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
