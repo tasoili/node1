b0c7d8d5609ab544f7210042b5f3c7fdddc98702
Tass Iliopoulos <tass@iliopoulos.com.au>
2012-12-12 07:01:14 -0500

    Added a router

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathName) {
+    console.log("In comes a request for " + pathName);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
+        var pathName = url.parse(request.url).pathName;
+        
+        route(pathName);
+        
         console.log("Request received");
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
     }
     
-    http.createServer(onRequest).listen(process.env.PORT || 8000, process.env.IP);
+    http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
     console.log("server has started");
 }
 
