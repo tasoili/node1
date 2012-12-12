b7fc519511103072c0351bbe6b660db801a9fcfb
Tass Iliopoulos <tass@iliopoulos.com.au>
2012-12-12 06:40:51 -0500

    Refactored

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8000);
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(process.env.PORT || 8000, process.env.IP);
+    console.log("server has started");
+}
+
+exports.start = start;
