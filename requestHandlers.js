306bb40f29bece3828868f8cbd9e4e053d29853a
Tass Iliopoulos <tass@iliopoulos.com.au>
2012-12-12 07:43:43 -0500

    External process

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,15 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
   
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
+  var content = "empty";
   
-  sleep(5000);
-  return "Hello Start";
+  exec("ls -lah", function (error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
 
 function upload() {
